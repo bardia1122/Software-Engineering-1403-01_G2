ce98d1983ba9dd78220652caa1496c2cf241286a
rozagp <rozaganjipour@gmail.com>
2025-01-09 22:38:29 +0330

    Function added for fixing delimiters

--- src/group3/logic.py
+++ src/group3/logic.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
-from hazm import Normalizer
-from .models import TextOptimization
+# from django.shortcuts import render
+# from hazm import Normalizer # type: ignore
+# from .models import TextOptimization
 
-def process_input(input_text):
-    normalizer = Normalizer()
-    optimized_text = normalizer.normalize(input_text)
-    return optimized_text
-    
+# def process_input(input_text):
+#     normalizer = Normalizer()
+#     optimized_text = normalizer.normalize(input_text)
+#     return optimized_text
+
+def fix_delimiters(input_text):
+    stack = []
+    output = []
+    match = {')': '(', '}': '{', ']': '['}
+
+    for c in input_text:
+        if c in match.values():
+            output.append(c)
+            stack.append(c)
+           
+        elif c in match.keys():
+            if stack and stack[-1] == match[c]:
+                stack.pop()
+                output.append(c)
+        else:
+            output.append(c)
+
+    while stack:
+        check = stack.pop()
+        output.append(')' if check == '(' else 
+                          '}' if check == '{' else ']')
+
+    return ''+(str)(output)
+print(fix_delimiters("uo)"))  
+
