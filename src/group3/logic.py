570779a9ca570bf6af04f12f09632ace19ddcd26
rozagp <rozaganjipour@gmail.com>
2025-01-09 22:42:40 +0330

    Type ignore removed

--- src/group3/logic.py
+++ src/group3/logic.py
@@ -11,24 +11,18 @@ def fix_delimiters(input_text):
     stack = []
     output = []
     match = {')': '(', '}': '{', ']': '['}
-
     for c in input_text:
         if c in match.values():
             output.append(c)
             stack.append(c)
-           
         elif c in match.keys():
             if stack and stack[-1] == match[c]:
                 stack.pop()
                 output.append(c)
         else:
             output.append(c)
-
     while stack:
         check = stack.pop()
         output.append(')' if check == '(' else 
                           '}' if check == '{' else ']')
-
-    return ''+(str)(output)
-print(fix_delimiters("uo)"))  
-
+    return ''.join(output)
